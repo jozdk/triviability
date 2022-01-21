253f0c8e925d6c9811de754a3d8a0911376f332a
jozdk <djannikhaezo@web.de>
2022-01-21 01:43:40 +0100

    Implement category selection

--- main_oop.js
+++ main_oop.js
@@ -1,6 +1,5 @@
-new Settings;
-new UI;
-const quiz = new Quiz();
+// new Settings;
+
 
 // Quiz Class: Represents a Quiz
 
@@ -11,9 +10,11 @@ class Quiz {
         this._questions = [];
     }
 
-    set categories(chosen) {
-        for (let cat of chosen) {
-            this.categories.push(cat);
+    set categories(selected) {
+        if (selected.request === "add") {
+            this._categories.push(selected.category);
+        } else if (selected.request === "remove") {
+            this._categories = this._categories.filter(cat => cat !== selected.category);
         }
     }
 
@@ -46,44 +47,83 @@ class Quiz {
 
 // Settings Class: Represents Settings
 
-class Settings {
-    constructor() {
+// class Settings {
+//     constructor() {
 
-    }
-}
+//     }
+// }
 
 // UI Class: Handles UI Tasks
 
 class UI {
     constructor() {
-        this.SelectionElement = document.querySelector("#category-selection");
+        this.selectionElement = document.querySelector("#category-selection");
         this.startButton = document.querySelector("#start-button");
-        this.catSelectElement.addEventListener("click", (event) => {
-            const category = event.target.firstElementChild.lastElementChild.innerText;
-            switch (category) {
-                case "Science":
-                    quiz.categories = "science";
-                case "History":
-                    quiz.categories = "history";
-                case "Geography":
-                    quiz.categories = "geography";
-                case "Movies":
-                    quiz.categories = "movies";
-                case "Art & Literature":
-                    quiz.categories = "literature";
-                case "Music":
-                    quiz.categories = "music";
-                case "Sport & Leisure":
-                    quiz.categories = "sport_and_leisure";
-                case "General Knowledge":
-                    quiz.categories = "general_knowledge";
-                case "Society & Culture":
-                    quiz.categories = "society_and_culture";
+        this.mainElement = document.querySelector("#main");
+        this.selectionElement.addEventListener("click", (event) => {
+            if (event.target !== this.selectionElement && !event.target.classList.contains("col-sm-12")) {
+                const elements = event.composedPath();
+                const targetElement = elements.find(element => element.classList.contains("category"));
+                const category = targetElement.firstElementChild.lastElementChild.innerText;
+                switch (category) {
+                    case "Science":
+                        this.toggleSelection(targetElement, "science");
+                        break;
+                    case "History":
+                        this.toggleSelection(targetElement, "history");
+                        break;
+                    case "Geography":
+                        this.toggleSelection(targetElement, "geography");
+                        break;
+                    case "Movies":
+                        this.toggleSelection(targetElement, "movies");
+                        break;
+                    case "Art & Literature":
+                        this.toggleSelection(targetElement, "literature");
+                        break;
+                    case "Music":
+                        this.toggleSelection(targetElement, "music");
+                        break;
+                    case "Sport & Leisure":
+                        this.toggleSelection(targetElement, "sport_and_leisure");
+                        break;
+                    case "General Knowledge":
+                        this.toggleSelection(targetElement, "general_knowledge");
+                        break;
+                    case "Society & Culture":
+                        this.toggleSelection(targetElement, "society_and_culture");
+                        break;
+                }
             }
+
         });
         this.startButton.addEventListener("click", () => {
             quiz.fetchQuestions();
-        })
+            this.mainElement.style.display = "none";
+            this.render();
+        });
+    }
+
+    toggleSelection(element, category) {
+        if (element.classList.contains("highlight")) {
+            element.classList.add("selected");
+            element.classList.remove("highlight");
+            quiz.categories = {
+                category: category,
+                request: "add"
+            };
+        } else if (element.classList.contains("selected")) {
+            element.classList.remove("selected");
+            element.classList.add("highlight");
+            quiz.categories = {
+                category: category,
+                request: "remove"
+            }
+        }
+    }
+
+    render() {
+        
     }
 
 }
@@ -92,3 +132,7 @@ class UI {
 // Local Storage Class: Handles Local Storage
 
 // Events?
+
+
+new UI;
+const quiz = new Quiz();
